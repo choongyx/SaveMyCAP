a2143e98ec55288df49da728b5f105ae597647b7
KerrynEer <KerrynEer@users.noreply.github.com>
2018-07-20 21:55:22 +0800

    Scroll button and shift add module to a new page

---
+++ client/js/add.js
@@ -0,0 +1,46 @@
+Template.add.rendered = function() {
+	$("#add-link").addClass('selected');
+	$("#myModules-link").removeClass('selected');
+	$("#login-link").removeClass('selected');
+	$("#profile-link").removeClass('selected');
+}
+
+Template.add.events({
+	"submit .add-module": function() {
+		var moduleCode = event.target.moduleCode.value;
+		var mc = event.target.mc.value;
+		var targetGrade = event.target.targetGrade.value;
+		var acadYear = event.target.acadYear.value;
+		var currentUser = Meteor.userId();
+
+		if (isNotEmpty(moduleCode) &&
+			isNotEmpty(targetGrade) &&
+			isNotEmpty(mc) && 
+			isNotEmpty(acadYear)) {
+			
+			Meteor.call('addModule', moduleCode, targetGrade, mc, acadYear);
+			Meteor.call('updateCAP', targetGrade, currentUser, mc);
+
+			event.target.moduleCode.value ="";
+			event.target.mc.value ="";
+			event.target.targetGrade.value ="";
+			event.target.acadYear.value ="";
+			
+			Bert.alert("Your Module Was Added!", "success", "growl-top-right");
+
+		} else {
+			Bert.alert("Something went wrong", "danger", "growl-top-right");
+		}
+		return false;
+	}
+});
+
+// Validation rules
+
+var isNotEmpty = function(value){
+	if(value && value != ''){
+		return true;
+	}
+	Bert.alert("Please fill in all fields", "danger", "growl-top-right");
+	return false;
+}
